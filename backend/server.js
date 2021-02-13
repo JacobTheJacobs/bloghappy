ef1451c3bacd0b9b49f1654881af52c66bae2a70
Master J <jake.yanov@gmail.com>
2021-02-13 20:33:02 +0200

    make it better

--- backend/server.js
+++ backend/server.js
@@ -43,8 +43,8 @@ if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "frontend", "build")));
   //app.use(cors(`${process.env.PPRODUCTION_URL}`));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
   });
 }
 //port
